9f6fe10ac1e4d1e5acb0d99da5621da9e5531d1c
Israel Adetunji <adisen230@gmail.com>
2021-01-07 14:56:22 +0100

    Updated port for prod

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.get('/', (req, res) => {
 
 app.use('/api/search', require('./routes/search'))
 
-const port = process.env | 5000
+const port = process.env.PORT || 5000
 app.listen(port, () => {
   console.log(`App listening at port ${port}`)
 })
